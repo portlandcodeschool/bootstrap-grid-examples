6813e7c8c32a56eb5e381677f17a585ebcc3d74e
David Manning <dlmanning@me.com>
2013-12-14 12:53:51 -0800

    more busey

--- js/site.js
+++ js/site.js
@@ -3,6 +3,21 @@
 window.addEventListener('resize', getViewportSize);
 document.addEventListener('DOMContentLoaded', getViewportSize);
 
+document.addEventListener('DOMContentLoaded', function () {
+  arr = document.getElementsByClassName('box');
+
+  for (var i = arr.length - 1; i >= 0; i--) {
+    arr[i].addEventListener('mouseover', function () {
+      this.id += this.id ? ' busey' : 'busey';
+    }, false);
+
+    arr[i].addEventListener('mouseleave', function () {
+      this.id = this.id.replace(/busey/, '');
+    }, false);
+  }
+
+})
+
 function getViewportSize () {
   var viewportWidth  = document.documentElement.clientWidth
     , viewportHeight = document.documentElement.clientHeight;
